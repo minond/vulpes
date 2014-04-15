da962c0956a057df26d2a2e4612c38136e70d2ea
Marcos Minond <minond.marcos@gmail.com>
2014-04-14 23:59:21 -0600

    loading routes from yaml file

--- scripts/server.js
+++ scripts/server.js
@@ -1,22 +1,41 @@
 'use strict';
 
-var vulpes, server, router;
+var vulpes, server, router, routes, fs, yaml, log, _;
 
 vulpes = require('vulpes');
+yaml = require('js-yaml');
+fs = require('fs');
+_ = require('lodash');
 
 router = new vulpes.Router();
 server = new vulpes.Server();
 
-router.routes.push(new vulpes.route.StaticRoute({
-    url: '/public/(.+)',
-    dir: './public'
-}));
+log = vulpes.logger('init');
+log.art.fox(log);
+log.info('starting vulpes application');
 
-router.routes.push(new vulpes.route.ControllerRoute({
-    url: '/users',
-    controller: 'Users',
-    action: 'edit'
-}));
+// load app routes
+log.info('loading routes');
+routes = yaml.safeLoad(fs.readFileSync(vulpes.std.path.routes, 'utf8'));
+_(routes).each(function (info, url) {
+    var route;
 
+    switch (true) {
+        case 'controller' in info:
+            log.debug('saving "%s" as a controller route', url);
+            route = vulpes.route.ControllerRoute;
+            break;
+
+        case 'dir' in info:
+            log.debug('saving "%s" as a static route', url);
+            route = vulpes.route.StaticRoute;
+            break;
+    }
+
+    info.url = url;
+    router.routes.push(new route(info));
+});
+
+log.info('completed initializing app');
 server.set_router(router);
 server.start();
