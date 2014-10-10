85473ea29f157b3fb2ffcd78b9754712365863b9
Marcos Minond <minond.marcos@gmail.com>
2014-10-09 20:32:50 -0600

    routes now defined in lowest level of routes.yml file

--- src/server.js
+++ src/server.js
@@ -6,12 +6,13 @@ var app = require('express')(),
 
 var join = require('path').join,
     format = require('util').format,
+    filter = require('lodash-node/modern/collections/filter'),
     map = require('lodash-node/modern/collections/map');
 
 config.$paths.push(join(__dirname, '..', 'config'));
 
 // define static files first. should not require middleware
-map(config.get('routes.static'), function (dir, url) {
+map(config.get('routes.=static'), function (dir, url) {
     log('static route %s (%s)', url, dir);
     app.use(url, require('serve-static')(dir));
 });
@@ -24,13 +25,16 @@ app.use(require('body-parser').json());
 app.use(require('cookie-parser')(config.get('application.cookies.secret')));
 
 // application routes
-map(config.get('routes.routes'), function (route, url) {
+map(filter(config.get('routes'), function (route, url) {
+    route.url = url;
+    return url[0] !== '=';
+}), function (route) {
     var method = route.method || config.get('structure.controllers.method'),
         action = route.action || config.get('structure.controllers.action'),
         controller = require(format(config.get('structure.server.controllers'), route.controller));
 
-    log('dynamic route %s (%s#%s)', url, route.controller, action);
-    app[ method ](url, controller[ action ]);
+    log('dynamic route %s (%s#%s)', route.url, route.controller, action);
+    app[ method ](route.url, controller[ action ]);
 });
 
 if (process.env.NODE_ENV === 'development') {
@@ -38,7 +42,7 @@ if (process.env.NODE_ENV === 'development') {
     require('swig').setDefaults({ cache: false });
     require('errorhandler').title = 'Vulpes';
 
-    map(config.get('routes.static'), function (dir, url) {
+    map(config.get('routes.=static'), function (dir, url) {
         log('serving index %s (%s)', url, dir);
         app.use(url, require('serve-index')(dir));
     });
