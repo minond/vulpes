5ecf3b1be256459158772b884d8c89a9d11d1532
Marcos Minond <minond.marcos@gmail.com>
2014-10-17 00:28:20 -0600

    adding mounts

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var app = require('express')(),
+var DEBUG = process.env.NODE_ENV === 'development',
+    make;
+
+var express = require('express'),
+    app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
     log = require('debug')('vulpes:server'),
@@ -11,49 +15,84 @@ var join = require('path').join,
     filter = require('lodash-node/modern/collections/filter'),
     map = require('lodash-node/modern/collections/map');
 
+function views(app, dir) {
+    app.set('view engine', 'html');
+    app.set('views', dir + '/assets/views/');
+    app.engine('html', require('swig').renderFile);
+}
+
+function static_routes(app, dir, config) {
+    map(config.get('routes.=static'), function (path, url) {
+        path = join(dir, path);
+
+        log('static route %s (%s)', url, path);
+        app.use(url, require('serve-static')(path));
+
+        if (DEBUG) {
+            log('serving index %s (%s)', url, path);
+            app.use(url, require('serve-index')(path));
+        }
+    });
+}
+
+function dynamic_routes(app, dir, base, routes) {
+    map(filter(routes, function (route, url) {
+        route.url = url;
+        return url[0] !== '=';
+    }), function (route) {
+        var method, action, controller;
+
+        if (route.mount) {
+            log('mounting %s on %s', route.mount, route.url);
+
+            app.use(make(
+                express(),
+                route.mount,
+                route.url,
+                new config.Configuration({ paths: [
+                    join(route.mount, 'config'),
+                    join(route.mount, 'node_modules', 'vulpes', 'config')
+                ] })
+            ));
+
+            return;
+        }
+
+        method = route.method || 'get';
+        action = route.action || 'index';
+        route.url = base + route.url;
+
+        log('dynamic route %s (%s#%s)', route.url, route.controller, action);
+        controller = require(format('%s/app/controllers/%s.js', dir, route.controller));
+        app[ method ](route.url, controller[ action ]);
+    });
+}
+
+function make(app, dir, base, config) {
+    static_routes(app, dir, config);
+    views(app, dir);
+    dynamic_routes(app, dir, base, config.get('routes'));
+    return app;
+}
+
 config.$paths.push(join(__dirname, '..', 'config'));
 
-// define static files first. should not require middleware
-map(config.get('routes.=static'), function (dir, url) {
-    log('static route %s (%s)', url, dir);
-    app.use(url, require('serve-static')(dir));
-});
-
-app.set('view engine', 'html');
-app.set('views', process.cwd() + '/assets/views/');
-app.engine('html', require('swig').renderFile);
 app.use(require('body-parser').urlencoded({ extended: false }));
 app.use(require('body-parser').json());
 app.use(require('cookie-parser')(config.get('application.cookies.secret')));
 app.use(function (req, res, next) { req.io = io; return next(); });
 
-// application routes
-map(filter(config.get('routes'), function (route, url) {
-    route.url = url;
-    return url[0] !== '=';
-}), function (route) {
-    var method = route.method || config.get('structure.controllers.method'),
-        action = route.action || config.get('structure.controllers.action'),
-        controller = require(format(config.get('structure.server.controllers'), route.controller));
+make(app, process.cwd(), '', config);
 
-    log('dynamic route %s (%s#%s)', route.url, route.controller, action);
-    app[ method ](route.url, controller[ action ]);
-});
-
-if (process.env.NODE_ENV === 'development') {
+if (DEBUG) {
     log('detected development enviroment');
     require('swig').setDefaults({ cache: false });
     require('errorhandler').title = require(process.cwd() + '/package.json').name;
-
-    map(config.get('routes.=static'), function (dir, url) {
-        log('serving index %s (%s)', url, dir);
-        app.use(url, require('serve-index')(dir));
-    });
-
     app.set('view cache', false);
     app.use(require('errorhandler')());
 }
 
 app.use(require('pageview')(app.get('views')));
 app.use(require('not-found')(app.get('views') + '404.html'));
+
 server.listen(process.env.PORT || 5000);
