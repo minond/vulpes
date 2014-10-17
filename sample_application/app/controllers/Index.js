51c452941a06450d10bd2412aade019c17bfbaf4
Marcos Minond <minond.marcos@gmail.com>
2014-10-17 15:59:21 -0600

    formatting json in view

--- sample_application/app/controllers/Index.js
+++ sample_application/app/controllers/Index.js
@@ -3,13 +3,13 @@
 module.exports = {
     dump: function dump(req, res) {
         res.render('dump', {
-            routes: JSON.stringify(req.config.get('routes'), null, '  '),
-            route: JSON.stringify(req.route, null, '  '),
-            headers: JSON.stringify(req.headers, null, '  '),
-            request: JSON.stringify({
+            routes: req.config.get('routes'),
+            route: req.route,
+            headers: req.headers,
+            request: {
                 params: req.params,
                 query: req.query,
-            }, null, '  '),
+            },
         });
     }
 };
