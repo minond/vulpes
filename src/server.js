f9541253d754be974bddecd8d5d17a5fd83bf6a1
Marcos Minond <minond.marcos@gmail.com>
2014-10-23 23:05:25 -0600

    breaking up the server file into smaller modules

--- src/server.js
+++ src/server.js
@@ -1,85 +1,27 @@
 'use strict';
 
-var DEBUG = process.env.NODE_ENV === 'development',
-    pkg = require(process.cwd() + '/package.json'),
-    make;
+var pkg = require(process.cwd() + '/package.json'),
+    make = require('./application');
 
 var express = require('express'),
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server),
-    log = require('debug')('vulpes:server'),
     config = require('acm');
 
-var join = require('path').join,
-    format = require('util').format,
-    filter = require('lodash-node/modern/collections/filter'),
-    map = require('lodash-node/modern/collections/map');
+var join = require('path').join;
 
-function views(app, dir) {
-    app.set('view engine', 'html');
-    app.set('views', dir + '/assets/views/');
-    app.engine('html', require('swig').renderFile);
-}
-
-function static_routes(app, dir, config) {
-    map(config.get('routes.=static'), function (path, url) {
-        path = join(dir, path);
-
-        log('static route %s (%s)', url, path);
-        app.use(url, require('serve-static')(path));
-
-        if (DEBUG) {
-            log('serving index %s (%s)', url, path);
-            app.use(url, require('serve-index')(path));
-        }
-    });
-}
-
-function dynamic_routes(app, dir, base, routes) {
-    map(filter(routes, function (route, url) {
-        route.url = url;
-        return url[0] !== '=';
-    }), function (route) {
-        var handler, func;
-
-        if (route.mount) {
-            log('mounting %s on %s', route.mount, route.url);
-
-            app.use(make(
-                express(),
-                route.mount,
-                base + route.url,
-                new config.Configuration({ paths: [
-                    join(route.mount, 'config'),
-                    join(route.mount, 'node_modules', 'vulpes', 'config')
-                ] })
-            ));
-
-            return;
-        } else {
-            log('dynamic route %s handled by %s', route.url, route.handler);
-
-            func = route.handler.split('#').pop();
-            handler = route.handler.split('#').shift();
-            handler = require(format('%s/app/%s.js', dir, handler));
-            app[ route.method || 'get' ](base + route.url, handler[ func ]);
-        }
-    });
-}
-
-function make(app, dir, base, config) {
-    static_routes(app, dir, config);
-    views(app, dir);
-    dynamic_routes(app, dir, base, config.get('routes'));
-    return app;
-}
+var body_parser = require('body-parser'),
+    cookie_parser = require('cookie-parser'),
+    not_found = require('not-found'),
+    page_view = require('pageview'),
+    error_handler = require('errorhandler');
 
 config.$paths.push(join(__dirname, '..', 'config'));
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
+app.use(cookie_parser(config.get('application.cookies.secret')));
 
-app.use(require('body-parser').urlencoded({ extended: false }));
-app.use(require('body-parser').json());
-app.use(require('cookie-parser')(config.get('application.cookies.secret')));
 app.use(function (req, res, next) {
     req.io = io;
     req.pkg = pkg;
@@ -89,15 +31,11 @@ app.use(function (req, res, next) {
 
 make(app, process.cwd(), '', config);
 
-if (DEBUG) {
-    log('detected development enviroment');
-    require('swig').setDefaults({ cache: false });
-    require('errorhandler').title = pkg.name;
-    app.set('view cache', false);
-    app.use(require('errorhandler')());
+if (process.env.NODE_ENV === 'development') {
+    error_handler.title = pkg.name;
+    app.use(error_handler());
 }
 
-app.use(require('pageview')(app.get('views')));
-app.use(require('not-found')(app.get('views') + '404.html'));
-
+app.use(page_view(app.get('views')));
+app.use(not_found(app.get('views') + '404.html'));
 server.listen(process.env.PORT || 5000);
