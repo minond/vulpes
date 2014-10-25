f36468e83e25bd1686a24d83ddba716b831ed28e
Marcos Minond <minond.marcos@gmail.com>
2014-10-25 13:45:56 -0600

    creating sub app in basic example

--- samples/basic/app/basic/controllers/index.js
+++ samples/basic/app/basic/controllers/index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     dump: function dump(req, res) {
-        res.render('dump', {
+        res.render('basic/dump', {
             routes: req.config.get('routes'),
             route: req.route,
             headers: req.headers,
