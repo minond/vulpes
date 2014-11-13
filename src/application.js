4587c7b4c8049f362378139b7589c1affd9c6a3a
Marcos Minond <minond.marcos@gmail.com>
2014-11-13 00:11:14 -0700

    staring unit tests

--- src/application.js
+++ src/application.js
@@ -263,22 +263,3 @@ module.exports = {
     serve_views: serve_views,
     static_routes: static_routes,
 };
-// var build = require('./application').build,
-//     join = require('path').join;
-//
-// var express = require('express'),
-//     app = express(),
-//     server = require('http').Server(app),
-//     io = require('socket.io')(server),
-//     config = require('acm');
-//
-// app.use(function (req, res, next) {
-//     req.io = io;
-//     return next();
-// });
-//
-// config.$paths.push(join(__dirname, '..', 'config'));
-// build(app, process.cwd(), '', config, process.env.NODE_ENV === 'development');
-// server.listen(process.env.PORT || 5000);
-
-
--- tests/application_test.js
+++ tests/application_test.js
@@ -1,12 +1,34 @@
 describe('Builder', function () {
     'use strict';
 
+    var app, req, res;
+
+    var map = require('lodash-node/modern/collections/map'),
+        noop = require('lodash-node/modern/utilities/noop');
+
     var expect = require('expect.js'),
+        express = require('express'),
         application = require('../src/application');
 
-    describe('vulpes', function () {
+    beforeEach(function () {
+        app = express();
+
+        req = {};
+        res = {};
+
+        req.url = 'hihihi';
+        res.setHeader = noop;
+    });
+
+    describe('#available_in_request', function () {
         it('?', function () {
-            expect(true).to.be(true);
+            application.available_in_request(app, true, 'hihihi');
+
+            map(app._router.stack, function (middleware) {
+                middleware.handle(req, res, noop);
+            });
+
+            expect(req.hihihi).to.be(true);
         });
     });
 });
